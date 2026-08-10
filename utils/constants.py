1348e7dcd30be2d1910f72b250979ee90b8a7840
Vanshaj Verma <vansh@Vanshajs-MacBook-Air-3.local>
2026-08-11 00:17:59 +0530

    feat: add interview loop constants (STEPS update, MAX_TECHNICAL_QUESTIONS, behavioral templates)

--- utils/constants.py
+++ utils/constants.py
@@ -1,15 +1,12 @@
 STEPS = [
-    "greeting",
-    "ask_name",
-    "ask_email",
-    "ask_phone",
-    "ask_location",
-    "ask_experience",
-    "ask_role",
-    "ask_tech_stack",
-    "confirm_tech_stack",
-    "generate_questions",
-    "end"
+    "greeting", "ask_name", "ask_email", "ask_phone", "ask_location",
+    "ask_experience", "ask_role", "ask_tech_stack", "confirm_tech_stack",
+    "interviewing", "end"
 ]
-
 EXIT_KEYWORDS = {"exit", "quit", "stop", "goodbye", "bye"}
+
+MAX_TECHNICAL_QUESTIONS = 4
+BEHAVIORAL_QUESTION_TEMPLATES = [
+    "What draws you to the {role} role, and where do you see it fitting into your career goals?",
+    "What led you to choose your field of study, and how has it shaped the way you approach {role_lower} work?",
+]
